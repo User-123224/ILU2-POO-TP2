01c957a872ad1b36d82a74f2d08344f2a6836b66
User-123224 <samuel.guillon1@gmail.com>
2025-11-07 15:29:26 +0100

    TP2_Test_v2

--- tests/controleur/ControlPrendreEtalTest.java
+++ tests/controleur/ControlPrendreEtalTest.java
@@ -5,15 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import personnages.Chef;
+import villagegaulois.Village;
+
 class ControlPrendreEtalTest {
+	private ControlPrendreEtal cpe;
+	private ControlVerifierIdentite cvi;
+	private ControlEmmenager cm;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		Village village = new Village("LeVillage", 10, 5);
+		village.setChef(new Chef("LeChef", 1, village));
+		cvi = new ControlVerifierIdentite(village);
+		cpe = new ControlPrendreEtal(cvi, village);
+		cm = new ControlEmmenager(village);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIdentite() {
+		cm.ajouterGaulois("Bonemine", 1);
+		assertTrue(cpe.verifierIdentite("Bonemine"));
+	}
+	
+	@Test
+	void testResteEtals() {
+		assertTrue(cpe.resteEtals());
 	}
 
 }
